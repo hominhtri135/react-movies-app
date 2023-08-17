9d50f88351610fd359a18b9986530e45c23cbc95
Minh Tri <hominhtri135@gmail.com>
2023-08-17 15:53:51 +0700

    add Code splitting Routes

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
-import { Fragment } from "react";
+import { Fragment, lazy, Suspense } from "react";
 import "swiper/scss";
 import "react-loading-skeleton/dist/skeleton.css";
-import Banner from "./components/banner/Banner";
-import HomePage from "./pages/HomePage";
+import Banner from "components/banner/Banner";
 import { Route, Routes } from "react-router-dom";
-import Main from "./components/layout/Main";
-import MoviePage from "./pages/MoviePage";
-import MovieDetailsPage from "./pages/MovieDetailsPage";
+import Main from "components/layout/Main";
+
+// dynamic imports
+const HomePage = lazy(() => import("pages/HomePage"));
+const MoviePage = lazy(() => import("pages/MoviePage"));
+const MovieDetailsPage = lazy(() => import("pages/MovieDetailsPage"));
 
 function App() {
   return (
     <Fragment>
-      <Routes>
-        <Route element={<Main></Main>}>
-          <Route
-            path="/react-movies-app"
-            element={
-              <>
-                <Banner></Banner>
-                <HomePage></HomePage>
-              </>
-            }
-          ></Route>
-          <Route
-            path="/react-movies-app/movies"
-            element={<MoviePage></MoviePage>}
-          ></Route>
-          <Route
-            path="/react-movies-app/movie/:movieId"
-            element={<MovieDetailsPage></MovieDetailsPage>}
-          ></Route>
-        </Route>
-      </Routes>
+      <Suspense fallback={<></>}>
+        <Routes>
+          <Route element={<Main></Main>}>
+            <Route
+              path="/react-movies-app"
+              element={
+                <>
+                  <Banner></Banner>
+                  <HomePage></HomePage>
+                </>
+              }
+            ></Route>
+            <Route
+              path="/react-movies-app/movies"
+              element={<MoviePage></MoviePage>}
+            ></Route>
+            <Route
+              path="/react-movies-app/movie/:movieId"
+              element={<MovieDetailsPage></MovieDetailsPage>}
+            ></Route>
+          </Route>
+        </Routes>
+      </Suspense>
     </Fragment>
   );
 }
